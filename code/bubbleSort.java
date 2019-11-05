67841e5679ce35acc2b7605c96dcd81bc1eea167
Pablo Sánchez <p.sanchez@unican.es>
2019-11-05 19:04:33 +0100

    Añadir Listados de Código

---
+++ code/bubbleSort.java
@@ -0,0 +1,18 @@
+public class BubbleSortExample {  
+
+  static void bubbleSort(int[] arr) {  
+
+    int n = arr.length;  
+    int temp = 0;  
+    
+    for(int i=0; i < n; i++){  
+      for(int j=1; j < (n-i); j++){  
+        if(arr[j-1] > arr[j]){  
+          temp = arr[j-1];  
+          arr[j-1] = arr[j];  
+          arr[j] = temp;  
+        } // if  
+      } //for 
+    } //for 
+  } // bubbleSort 
+} // class
